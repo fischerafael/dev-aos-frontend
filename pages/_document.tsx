949be0ee513c84c27f8bbfdcfc0a6bee158b897c
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-04-23 16:28:45 -0300

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,6 +37,7 @@ export default class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700;900&display=swap"
                         rel="stylesheet"
                     />
+                    <link rel="shortcut icon" href="/assets/logo.svg" />
                 </Head>
                 <body>
                     <Main />
